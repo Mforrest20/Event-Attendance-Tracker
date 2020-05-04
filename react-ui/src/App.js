563019e6bbcb5dc5aed275e3e655bc61401f11d6
Noah Lone <noah.lonen@gmail.com>
2020-05-04 15:52:43 -0400

    added create event stuff

--- react-ui/src/App.js
+++ react-ui/src/App.js
@@ -1,23 +1,24 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import Events from './component/EventsList';
+import CreateEvent from './component/CreateEvent';
 
 
-export default class App extends Component{
+export default class App extends Component {
 
-  state={
+  state = {
     'eventsLists': [
-      {'name': 'GSU orientation 2020(vertual)', 'id': 0},
-      {'name': 'FreshMan orientation 2020', 'id':1},
-      {'name': 'Study Group', 'id': 2}
+      { 'name': 'GSU orientation 2020(vertual)', 'id': 0 },
+      { 'name': 'FreshMan orientation 2020', 'id': 1 },
+      { 'name': 'Study Group', 'id': 2 }
     ]
   }
 
   render() {
     return (
       <div className='App'>
-
-      <h1>GSU.events</h1>
-      <Events eventsLists={this.state.eventsLists}></Events>
+        <h1>GSU.events</h1>
+        <Events eventsLists={this.state.eventsLists}></Events>
+        <CreateEvent />
       </div>
     );
   }
---
+++ react-ui/src/component/CreateEvent.js
@@ -0,0 +1,54 @@
+import React from 'react';
+class CreateEvent extends React.Component {
+
+    state = { showForm: false }
+
+    showForm = () => {
+        return (
+            <div className="form-style-6">
+                <form id="add-app">
+
+                    <label>Event Name: </label>
+                    <input type="text"></input>
+
+                    <label>Semester: </label>
+                    <input type="text" ></input>
+
+                    <label>Year: </label>
+                    <input type="number"></input>
+
+                    <label>Description (Optional): </label>
+                    <textarea placeholder="Type your Message.."></textarea>
+
+                    <label>Start Date: </label>
+                    <input type="date"></input>
+
+                    <label>End Date: </label>
+                    <input type="date"></input>
+
+                    <label>Semester: </label>
+                    <input type="text" ></input>
+
+                    <label>Start Time: </label>
+                    <input type="time" ></input>
+
+                    <label>End Time: </label>
+                    <input type="time" ></input>
+
+                    <button>Create</button>
+                </form>
+            </div>
+        );
+    }
+    render() {
+        return (
+            <div className='create-event'>
+                <h1>Manage Application</h1>
+                <button onClick={() => this.setState({ showForm: true })}>Create Event</button>
+                {this.state.showForm ? this.showForm() : null}
+            </div>
+        );
+    }
+}
+
+export default CreateEvent;
