4e69e367df094208dbfb54f58b82f1519e0ef469
justcallmeAman <Amanuelzelealem@gmail.com>
2020-05-04 15:08:03 -0400

    react frontend code/eventList

---
+++ react-ui/src/App.js
@@ -0,0 +1,24 @@
+import React, {Component} from 'react';
+import Events from './component/EventsList';
+
+
+export default class App extends Component{
+
+  state={
+    'eventsLists': [
+      {'name': 'GSU orientation 2020(vertual)', 'id': 0},
+      {'name': 'FreshMan orientation 2020', 'id':1},
+      {'name': 'Study Group', 'id': 2}
+    ]
+  }
+
+  render() {
+    return (
+      <div className='App'>
+
+      <h1>GSU.events</h1>
+      <Events eventsLists={this.state.eventsLists}></Events>
+      </div>
+    );
+  }
+}
