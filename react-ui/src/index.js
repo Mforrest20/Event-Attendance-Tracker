788c16cf007ae15c7952b4f53a65af91e4f91db8
ItzWorm <mforrest10@gmail.com>
2020-05-04 22:08:45 -0400

    fixed my fuck up I think

--- react-ui/src/index.js
+++ react-ui/src/index.js
@@ -4,11 +4,16 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+
+
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+
+
+    document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
