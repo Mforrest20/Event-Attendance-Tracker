98bf8908f73c136bfa748b660169059ac823ddd0
ItzWorm <mforrest10@gmail.com>
2020-05-04 21:21:31 -0400

    This commit is occurring after a delete and install of react environment.

--- react-ui/src/App.js
+++ react-ui/src/App.js
@@ -1,24 +1,26 @@
-import React, {Component} from 'react';
-import Events from './component/EventsList';
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
-
-export default class App extends Component{
-
-  state={
-    'eventsLists': [
-      {'name': 'GSU orientation 2020(vertual)', 'id': 0},
-      {'name': 'FreshMan orientation 2020', 'id':1},
-      {'name': 'Study Group', 'id': 2}
-    ]
-  }
-
-  render() {
-    return (
-      <div className='App'>
-
-      <h1>GSU.events</h1>
-      <Events eventsLists={this.state.eventsLists}></Events>
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
+
+export default App;
